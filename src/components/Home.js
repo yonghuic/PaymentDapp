b34ea4ae1d7748e980fcceb03681630c2e44b1fb
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-06-26 12:28:11 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Home.css"
 
-
 function Home () {
     return (
        <>
-       <div className="logoimage"><img src={`${process.env.PUBLIC_URL}/img/charlie1.jpg`} alt="pay"/></div>
+       <div className="logoimage"><img src={`${process.env.PUBLIC_URL}/img/logo.png`} alt="pay"/></div>
        </> 
     )
 }
