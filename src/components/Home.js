d53f263d76da73676d3e3e95e37449f108e8b644
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-06-26 11:22:10 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import "./Home.css"
 function Home () {
     return (
        <>
-       <div className="logoimage"><img src="https://yonghuic.github.io/PaymentDapp/public/logo.png?raw=true" alt="pay"/></div>
+       <div className="logoimage"><img src="PaymentDapp/public/logo.png" alt="pay"/></div>
        </> 
     )
 }
