be77fbb6a4b4b32277f55145524a11a4f44ff40e
yonghuic <75155638+yonghuic@users.noreply.github.com>
2022-06-26 11:40:49 +0800

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ import "./Home.css"
 function Home () {
     return (
        <>
-       <div className="logoimage"><img src="%PUBLIC_URL%/logo.png" alt="pay"/></div>
+       <div className="logoimage"><img src={process.env.PUBLIC_URL + "/logo.png"} alt="pay"/></div>
        </> 
     )
 }
